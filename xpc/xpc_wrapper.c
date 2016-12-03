63a26e8bb1b0db7067d302aa02384e835144f16e
Dimitri Sokolyuk <demon@dim13.org>
2016-12-03 20:35:49 +0100

    KNF

--- xpc/xpc_wrapper.c
+++ xpc/xpc_wrapper.c
@@ -35,61 +35,60 @@ const ptr_to_uuid_t ptr_to_uuid(void *p) { return (ptr_to_uuid_t)p; }
 xpc_connection_t
 XpcConnect(char *service, uintptr_t ctx)
 {
-    dispatch_queue_t queue = dispatch_queue_create(service, 0);
-    xpc_connection_t conn = xpc_connection_create_mach_service(service, queue, XPC_CONNECTION_MACH_SERVICE_PRIVILEGED);
-    // making a local copy, that should be made "persistent" with the following Block_copy
-    //GoInterface ictx = *((GoInterface*)ctx);
-    xpc_connection_set_event_handler(conn,
-        Block_copy(^(xpc_object_t event) {
-            handleXpcEvent(event, ctx); //(void *)&ictx);
-        })
-    );
-
-    xpc_connection_resume(conn);
-    return conn;
+	dispatch_queue_t queue = dispatch_queue_create(service, 0);
+	xpc_connection_t conn = xpc_connection_create_mach_service(service, queue, XPC_CONNECTION_MACH_SERVICE_PRIVILEGED);
+	// making a local copy, that should be made "persistent" with the following Block_copy
+	//GoInterface ictx = *((GoInterface*)ctx);
+	xpc_connection_set_event_handler(conn,
+		Block_copy(^(xpc_object_t event) {
+			handleXpcEvent(event, ctx); //(void *)&ictx);
+		})
+	);
+	xpc_connection_resume(conn);
+	return conn;
 }
 
 void
 XpcSendMessage(xpc_connection_t conn, xpc_object_t message, bool release, bool reportDelivery)
 {
-    xpc_connection_send_message(conn,  message);
-    xpc_connection_send_barrier(conn, ^{
-        // Block is invoked on connection's target queue
-        // when 'message' has been sent.
-        if (reportDelivery) { // maybe this could be a callback
-            puts("message delivered");
-        }
-    });
-    if (release) {
-        xpc_release(message);
-    }
+	xpc_connection_send_message(conn,  message);
+	xpc_connection_send_barrier(conn, ^{
+		// Block is invoked on connection's target queue
+		// when 'message' has been sent.
+		if (reportDelivery) { // maybe this could be a callback
+			puts("message delivered");
+		}
+	});
+	if (release) {
+		xpc_release(message);
+	}
 }
 
 void
 XpcArrayApply(uintptr_t v, xpc_object_t arr)
 {
-  xpc_array_apply(arr, ^bool(size_t index, xpc_object_t value) {
-    arraySet(v, index, value);
-    return true;
-  });
+	xpc_array_apply(arr, ^bool(size_t index, xpc_object_t value) {
+		arraySet(v, index, value);
+		return true;
+	});
 }
 
 void
 XpcDictApply(uintptr_t v, xpc_object_t dict)
 {
-  xpc_dictionary_apply(dict, ^bool(const char *key, xpc_object_t value) {
-    dictSet(v, (char *)key, value);
-    return true;
-  });
+	xpc_dictionary_apply(dict, ^bool(const char *key, xpc_object_t value) {
+		dictSet(v, (char *)key, value);
+		return true;
+	});
 }
 
 void
 XpcUUIDGetBytes(void *v, xpc_object_t uuid)
 {
-   const uint8_t *src = xpc_uuid_get_bytes(uuid);
-   uint8_t *dest = (uint8_t *)v;
+	const uint8_t *src = xpc_uuid_get_bytes(uuid);
+	uint8_t *dest = (uint8_t *)v;
 
-   for (int i=0; i < sizeof(uuid_t); i++) {
-     dest[i] = src[i];
-   }
+	for (int i=0; i < sizeof(uuid_t); i++) {
+		dest[i] = src[i];
+	}
 }
